7f4c2566f60a9d566d0890009e6b16d243cafb03
elcappo1991 <el.cappo@gmail.com>
2017-03-03 09:57:09 +0000

    add server vps 5

--- apps/Alexa_training_AV/index.js
+++ apps/Alexa_training_AV/index.js
@@ -4,8 +4,7 @@ module.change_code = 1;
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'Alexa_training_AV' );
 var req= require('request-promise')
-var http=require('http')
-var asyncronus=require('async')
+
 
 
 
@@ -62,13 +61,6 @@ app.intent("name", {
   function(request, response) {
   	var nameToRepeat= request.slot('NAMED')
     
-   /* http.get("http://localhost:5000", function(res) {
-      console.log('called')
-        // This is async and will run after the http call returns
-        response.say("hello");
-        // Must call send to end the original request
-        response.send();
-    });*/
 
 
 req.post({url:'http://localhost:5000', form:{key:nameToRepeat}},
@@ -79,14 +71,14 @@ req.post({url:'http://localhost:5000', form:{key:nameToRepeat}},
       console.log('found')
      
       response.say('founded')
-      response.send()
+      
       
  
        
     }else {
       console.log('not found')
           response.say('Not founded')
-      response.send()
+     
       
       
 
@@ -98,121 +90,16 @@ req.post({url:'http://localhost:5000', form:{key:nameToRepeat}},
           
           });
 
-
-  return false
-  /*
-    val=0
-
-
-   myfnToCallWebServic(nameToRepeat,function(){
-         console.log(val)
-        
-         return val
-
+return response.send()
+  //return false
+  
    
-
-     }).then(function() {
-      response.say('hello')
-      
-    });
-   
-   setTimeout(function(){console.log('delai taime')
-     say2(response,nameToRepeat,function(r){
-        
-        console.log('inside say function found')
-     
-       async response.send('hello')
-     
-    })  
-
- },4000)
-   
-      
-       say2(response,nameToRepeat,function(r){
-        
-        console.log('inside say function found')
-     
-       
-     
-    })  
-   */
 
    
 
 
 
 });
-
-function say2(response,nameToRepeat,callback){
-response.say('i am know looking for your device in the cloud')
-  response.say('Please wait')
-  console.log("*******",val)
-  if (val ==1){
-     callback( response.say(nameToRepeat+ " Speaker linked"));
-      }
-      else  {
-        callback(response.say(nameToRepeat+ " Speaker Not Found please check if the Speaker is connected"));
-    
-    }
-   
-
-}
-
-function say(response,nameToRepeat,callback){
-
-  req.post({url:'http://localhost:5000', form:{key:nameToRepeat}},
-   function(error, res, body) {
-   if (!error && res.statusCode == 200) {
-      console.log(body)
-    if (body=='found'){
-      console.log('found')
-     
-     callback("found");
-      
-      
- 
-       
-    }else {
-      console.log('not found')
-      
-      callback('not found')
-
-     
-    }
-    
-  }
-          
-          });
-
-}
-
-function myfnToCallWebServic(nameToRepeat,callback){
-
-    req.post({url:'http://localhost:5000', form:{key:nameToRepeat}},
-   function(error, res, body) {
-   if (!error && res.statusCode == 200) {
-      console.log(body)
-    if (body=='found'){
-      console.log('found')
-     
-      callback(val=1)
-      
- 
-       
-    }else {
-      console.log('not found')
-        callback(val =0)
-      
-
-
-     
-    }
-    
-  }
-          
-          });
-
-}
 
 
 
