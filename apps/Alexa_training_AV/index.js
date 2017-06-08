59a1196985dad008a4e3cfadc89f0b5127794d24
elcappo1991 <el.cappo@gmail.com>
2017-06-08 11:59:42 +0100

    select troubleshooting10

--- apps/Alexa_training_AV/index.js
+++ apps/Alexa_training_AV/index.js
@@ -813,6 +813,8 @@ app.intent("link", {
         i = 0
 
         speakerName = ''
+
+
         return http.getAsync({ url: 'https://oauth20.herokuapp.com/api/speakers', headers: { 'Authorization': reqheader }, json: true }).spread(function(statusCodesError, listspeakerConnected) {
 
 
@@ -820,7 +822,7 @@ app.intent("link", {
 
                 if (speaker.name == namespeakerfromalexa) {
                     var promise = new RSVP.Promise(function(fulfill, reject) {
-                        str = http.postAsync({ url: 'http://vps341573.ovh.net:5050/', json: true, form: { key: speaker.num_serie } },
+                        str = http.postAsync({ url: 'http://vps341573.ovh.net:5050/', json: true, form: { key: speaker.num_serie } }).spread(
 
                             function(error, resul, body) {
 
