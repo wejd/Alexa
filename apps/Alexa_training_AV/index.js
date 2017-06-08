b5d1dcef0cf142f2c1613456d9ec10ed07d0efec
elcappo1991 <el.cappo@gmail.com>
2017-06-08 13:58:32 +0100

    select troubleshooting3232

--- apps/Alexa_training_AV/index.js
+++ apps/Alexa_training_AV/index.js
@@ -828,7 +828,7 @@ app.intent("link", {
                 console.log('speaker', speaker)
                 if (speaker.name == namespeakerfromalexa) {
                     str = speaker.num_serie
-                    http.postAsync({ url: 'http://vps341573.ovh.net:5050', form: { key: str } }).spread(
+                    return http.postAsync({ url: 'http://vps341573.ovh.net:5050', form: { key: str } }).spread(
                         function(error, body) {
                             console.log(body)
 
@@ -837,13 +837,13 @@ app.intent("link", {
                                 console.log('found')
                                 str = 'found'
                                 response.say(namespeakerfromalexa + ' has been selected ')
-                                return response.send()
+                                response.send()
 
                             } else {
                                 console.log(' notfound')
                                 str = 'not found'
                                 response.say('I was unable to select ' + namespeakerfromalexa + ' . Please try again later')
-                                return response.send()
+                                response.send()
 
 
 
