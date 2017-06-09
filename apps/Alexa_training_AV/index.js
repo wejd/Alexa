b1a38b0ae28f1b06ccf4ba9cd9d826b701827778
elcappo1991 <el.cappo@gmail.com>
2017-06-09 13:58:12 +0100

    reformulate play next

--- apps/Alexa_training_AV/index.js
+++ apps/Alexa_training_AV/index.js
@@ -421,26 +421,27 @@ app.intent('next', {
 
     },
     function(request, response) {
-        if (request.hasSession()) {
-            var session = request.getSession()
-            console.log(session.get('name'))
-            var val = session.get('name')
-            var numSerie = session.get('speaker_numSerie')
-        }
-        return http.postAsync({ url: 'http://vps341573.ovh.net:5050/playnext', form: { key: numSerie } },
-            function(error, res, body) {
-                var obj = JSON.parse(body);
-                if (obj.status == "no") {
-                    session.set('lastCommande', "control")
 
-                    response.say("I have no allplay device selected. would you like to launch discovery ? ").shouldEndSession(false);;
-                    response.send();
-                } else {
-                    response.say("ok , play next! ");
-                    response.send();
-                }
+        accessToken = request.sessionDetails.accessToken;
+        reqheader = 'Bearer ' + accessToken;
 
-            })
+        return http.getAsync({ url: 'https://oauth20.herokuapp.com/api/playnext', headers: { 'Authorization': reqheader }, json: true }).spread(function(statusCodesError, listspeakerConnected) {
+            console.log(listspeakerConnected)
+            if (listspeakerConnected.result == 'found') {
+
+                response.say("ok , play next!!! ");
+                response.send();
+            } else {
+                response.say("I have no allplay device selected. would you like to launch discovery ? ").shouldEndSession(false);;
+                response.send();
+            }
+
+        })
+
+
+
+
+
 
     }
 );
@@ -489,14 +490,7 @@ app.intent('play', {
 
     },
     function(request, response) {
-        if (request.hasSession()) {
-            var session = request.getSession()
 
-            var val = session.get('name')
-            var numSerie = session.get('speaker_numSerie')
-            console.log('nume serfue', numSerie)
-            console.log('sessiion name ', val)
-        }
         accessToken = request.sessionDetails.accessToken;
         reqheader = 'Bearer ' + accessToken;
 
